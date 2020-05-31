3fc8ea3d2b35e5a1ff67f620e54d67817ee796bc
Sumit Govil <sumit.govil@optus.com.au>
2020-05-31 16:51:03 +1000

    Sign in implemented

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,48 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import jwt from "jsonwebtoken";
+import { validateRequest } from "../middlewares/validate-request";
+import { User } from "../models/user";
+import { BadRequestError } from "../errors/bad-request-error";
+import { Password } from "../services/password";
 
 const router = express.Router();
+const signinValidation = [
+  body("email").isEmail().withMessage("Email must be valid"),
+  body("password")
+    .trim()
+    .notEmpty()
+    .withMessage("Password must be between 4 and 20 characters"),
+];
 
-router.post("/api/users/signin", (req, res) => {
-  res.send("Hi there!");
-});
+router.post(
+  "/api/users/signin",
+  signinValidation,
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError("Invalid credentials");
+    }
+    const match = await Password.compare(existingUser.password, password);
+    if (!match) {
+      throw new BadRequestError("Invalid credentials");
+    }
+    //Generate token
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY! // This ! conveys to TS that we have already ensured this key is defined (in index.ts)
+    );
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(200).send(existingUser);
+  }
+);
 
 export { router as signinRouter };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,9 @@
 import express, { Request, Response } from "express";
 import jwt from "jsonwebtoken";
+import { body } from "express-validator";
 import { Password } from "../services/password";
-import { body, validationResult } from "express-validator";
-import { RequestValidationError } from "../errors/request-validation-error";
 import { BadRequestError } from "../errors/bad-request-error";
+import { validateRequest } from "../middlewares/validate-request";
 import { User } from "../models/user";
 
 const router = express.Router();
@@ -18,11 +18,8 @@ const signupValidation = [
 router.post(
   "/api/users/signup",
   signupValidation,
+  validateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
     const { email, password } = req.body;
 
     const existingUser = await User.findOne({ email });

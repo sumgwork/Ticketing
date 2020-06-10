fb244b6a8aee7a199cd5a749cbea89841382c644
Sumit Govil <sumit.govil@optus.com.au>
2020-06-10 17:06:37 +1000

    show route done

---
+++ orders/src/routes/__test__/show.test.ts
@@ -0,0 +1,58 @@
+import request from "supertest";
+import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
+import mongoose from "mongoose";
+
+const createTicket = async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+  return ticket;
+};
+
+const createOrder = async (ticket, user) => {
+  const order = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket._id })
+    .expect(201);
+  return order;
+};
+
+it("throws 404 if order is not found", async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+  await request(app)
+    .get(`/api/orders/${id}`)
+    .set("Cookie", global.signin())
+    .expect(404);
+});
+
+it("throws error if order does not belong to the user", async () => {
+  const ticket1 = await createTicket();
+  const user1 = global.signin();
+  const user2 = global.signin();
+
+  const { body: order1 } = await createOrder(ticket1, user1);
+
+  await request(app)
+    .get(`/api/orders/${order1.id}`)
+    .set("Cookie", user2)
+    .expect(401);
+});
+
+it("fetches details of a particular order", async () => {
+  const ticket1 = await createTicket();
+  const user1 = global.signin();
+
+  const { body: order1 } = await createOrder(ticket1, user1);
+
+  // Make request to get orders for user#2
+  const response = await request(app)
+    .get(`/api/orders/${order1.id}`)
+    .set("Cookie", user1)
+    .expect(200);
+
+  expect(response.body.id).toEqual(order1.id);
+});
--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,16 +1,28 @@
-import { NotFoundError } from "@sg-tickets/common";
+import {
+  NotAuthorisedError,
+  NotFoundError,
+  requireAuth,
+} from "@sg-tickets/common";
 import express, { Request, Response } from "express";
 import { Order } from "../models/order";
 
 const router = express.Router();
 
-router.get("/api/orders/:id", async (req: Request, res: Response) => {
-  const order = await Order.findById(req.params.id);
-  if (!order) {
-    throw new NotFoundError();
-  }
+router.get(
+  "/api/orders/:id",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.id).populate("ticket");
+    if (!order) {
+      throw new NotFoundError();
+    }
 
-  res.send(order);
-});
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorisedError();
+    }
+
+    res.send(order);
+  }
+);
 
 export { router as showOrderRouter };

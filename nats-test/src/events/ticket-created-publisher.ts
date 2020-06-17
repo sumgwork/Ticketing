f7bcf324cdb3e30082a63c0ce1455c5151a098f6
Sumit Govil <sumit.govil@optus.com.au>
2020-06-17 11:26:19 +1000

    adding deployments

--- nats-test/src/events/ticket-created-publisher.ts
+++
@@ -1,5 +0,0 @@
-import { Publisher, TicketCreatedEvent, Subjects } from "@sg-tickets/common";
-
-export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
-  subject: Subjects.TicketCreated = Subjects.TicketCreated;
-}
